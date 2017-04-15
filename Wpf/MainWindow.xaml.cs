fb85925d1a22b0919f2d8f7135dd4b4680c63745
Lusien Lozanov <des7royer.970420@gmail.com>
2017-04-15 03:11:10 +0300

    UI Init commit

---
+++ Wpf/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void ButtonNew_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+        private void ButtonClickRoom(object sender, RoutedEventArgs e)
+        {
+            this.contentControl.Content = new UserControlls.RoomUserControl();
+        }
+        private void ButtonClickStudents(object sender, RoutedEventArgs e)
+        {
+            this.contentControl.Content = new UserControlls.StudentUserControl();
+        }
+    }
+}
